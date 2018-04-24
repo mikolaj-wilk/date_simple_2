050173bfa4c15cc0ac8cc39d9f9e666bffc3ee43
mikolaj-wilk <37270202+mikolaj-wilk@users.noreply.github.com>
2018-04-24 07:39:58 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+setup(
+   name='date_simple',
+   version='1.0',
+   description='A homework assignment',
+   long_description=long_description,
+   author='Mikolaj Wilk',
+   author_email='mikolajwilk@gmail.com',
+   url="https://github.com/mikolaj-wilk/date_simple_2",
+   packages=['date_simple'],
+   install_requires=['datetime'])
